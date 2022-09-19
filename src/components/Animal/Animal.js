eafe59b59f7ea9c6e46638b06a57ff7e719b4685
Julie Nisbet <jmnisbet@gmail.com>
2022-09-19 09:38:37 -0700

    demo loading from static file

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,10 @@
-export default function Animal() {
-  return <></>;
+import './Animal.css';
+export default function Animal({ name, type, top, left, says }) {
+  return (
+    <div className="animal" style={{ top, left }}>
+      <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+      <span className="name">{name}</span>
+      <span>{says}</span>
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import './Main.css';
+import { animals } from '../../data';
+import Animal from '../Animal/Animal';
+import background from '../../background.png';
 
 export default function Main() {
-  return <main></main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal
+          key={animal.name}
+          // name={animal.name}
+          // type={animal.type}
+          // top={animal.top}
+          // left={animal.left}
+          // says={animal.says}
+          {...animal}
+        />
+      ))}
+    </main>
+  );
 }
